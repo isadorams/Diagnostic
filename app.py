d7156474aac96d6acf1c4f5c15187b721b7a1d86
isadorams <53095012+isadorams@users.noreply.github.com>
2022-05-19 16:18:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,16 +8,16 @@ st.write("# Classificação de Cancer")
 st.write("## Breast Cancer Wisconsin (Diagnostic)")
 
 st.sidebar.write("### Parâmetros")
-st.sidebar.slider("Perimeter", 49.0, 251.0, 107.0, 0.1)
-st.sidebar.slider("Area", 184.0, 4254.0, 881.0, 0.1)
-st.sidebar.slider("Compactness", 0.01, 1.6, 0.25, 0.1)
-st.sidebar.slider("Concavity", 0.0, 1.30, 0.27, 0.1)
+st.sidebar.slider("Perimeter", 40.0, 190.0, 100.0)
+st.sidebar.slider("Area", 184.0, 4254.0, 881.0)
+st.sidebar.slider("Compactness", 0.01, 1.6, 0.25)
+st.sidebar.slider("Concavity", 0.0, 1.30, 0.27)
     
 
 with open("objetos.pkl", "rb") as arquivo:
   ss, dtc = pickle.load(arquivo)
 
-  estrutura = {'perimeter': perimeter, 'area': area, 'compactness': compactness, 'concavity': concavity}
+  estrutura = ['perimeter', 'area', 'compactness', 'concavity']
   df = pd.DataFrame(estrutura, index=[0])
   
   st.write("### Parâmetros de Entrada")
