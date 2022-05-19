e47b1eb5146ad82f7fd97bed6db21ff3024adc51
isadorams <53095012+isadorams@users.noreply.github.com>
2022-05-19 16:04:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,5 +13,29 @@ st.sidebar.slider("Area", 184.0, 4254.0, 881.0, 0.1)
 st.sidebar.slider("Compactness", 0.01, 1.6, 0.25, 0.1)
 st.sidebar.slider("Concavity", 0.0, 1.30, 0.27, 0.1)
     
-    
+
+with open("objetos.pkl", "rb") as arquivo:
+  ss, dtc = pickle.load(arquivo)
+
+  estrutura = {'perimeter': perimeter, 'area': area, 'compactness': compactness, 'concavity': concavity}
+  df = pd.DataFrame(estrutura, index=[0])
+  
+  st.write("### Parâmetros de Entrada")
+  st.write(df)
+  
+  df = ss.transform(df)
+  st.write(df)
+  
+  predicao = dtc.predict(df)
+  st.write(f"A classe é: **{predicao[0]}**")
+  
+  predicao = dtc.predict_proba(df)
+  predicao = pd.DataFrame(predicao)
+  predicao.rename({
+     'M' : 0,
+     'B' : 1
+  }, axis=1, inplace=True)
+  
+  st.write("Probabilidades")
+  st.write(predicao)
 
