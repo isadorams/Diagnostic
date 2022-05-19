0c9137e10b21a29dba06c596b4338a8b308b7f8a
isadorams <53095012+isadorams@users.noreply.github.com>
2022-05-19 15:18:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.write("# Classificação de Cancer")
 st.write("## Breast Cancer Wisconsin (Diagnostic)")
 
 st.sidebar.write("### Parâmetros")
-st.sidebar.slider("Radios", 7.8, 36.0, 16.3, 0.1)
+st.sidebar.slider("Radius", 7.8, 36.0, 16.3, 0.1)
 st.sidebar.slider("Texture", 11.0, 50.0, 26.0, 0.1)
 st.sidebar.slider("Perimeter", 49.0, 251.0, 107.0, 0.1)
 st.sidebar.slider("Area", 184.0, 4254.0, 881.0, 0.1)
