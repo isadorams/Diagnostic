81b31aaceecb9589593cd2527d805504f34ef7ef
isadorams <53095012+isadorams@users.noreply.github.com>
2022-05-19 15:14:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pickle
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
+import pandas as pd
 
 st.write("# Classificação de Cancer")
 st.write("## Breast Cancer Wisconsin (Diagnostic)")
@@ -14,3 +18,31 @@ st.sidebar.slider("Concavity", 0.0, 1.30, 0.27, 0.1)
 st.sidebar.slider("Concave", 0.0, 0.30, 0.11, 0.1)
 st.sidebar.slider("Symmetry", 0.14, 0.66, 0.30, 0.1)
 st.sidebar.slider("Fractal", 0.054, 0.20, 0.08, 0.1)
+
+with open("objetos.pkl", "rb") as arquivo:
+  ss, dtc = pickle.load(arquivo)
+
+  estrutura = {'radius': radius, 'texture': texture, 'perimeter': perimeter, 'area': area, 'smoothness': smoothness,
+                   'compactness': compactness, 'concavity': concavity, 'concave': concave, 'symmetry': symmetry,
+                   'fractal': fractal }
+  df = pd.DataFrame(estrutura, index=[0])
+  
+  st.write("### Parâmetros de Entrada")
+  st.write(df)
+  
+  df = ss.transform(df)
+  st.write(df)
+  
+  predicao = dtc.predict(df)
+  st.write(f"A classe é: **{predicao[0]}**")
+  #st.write("**A classe dessa flor é:**")
+  #st.write(predicao)
+  predicao = dtc.predict_proba(df)
+  predicao = pd.DataFrame(predicao)
+  predicao.rename({
+     'M' : 0,
+     'B' : 1
+  }, axis=1, inplace=True)
+  
+  st.write("Probabilidades")
+  st.write(predicao)
